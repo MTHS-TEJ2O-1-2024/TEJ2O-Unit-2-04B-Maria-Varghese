2ae592b97e05079164f75eac9e751c9c0f2970b4
Maria-Varghese <maria.varghese@mths.ca>
2024-09-23 07:50:51 -0700

    made changes in temperature

--- main.ts
+++ main.ts
@@ -2,7 +2,17 @@
  *
  * Created by: Mr. Coxall
  * Created on: Sep 2020
- * This program ...
+ * This program shows the temperature of a place
 */
 
-basic.showString('Hello, World!')
+//our variable for temperature
+let temperature: Number
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    basic.showString('Temperature is')
+    temperature = input.temperature()
+    basic.showNumber (input.temperature())
+})
